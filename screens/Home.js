a0b5065068636a9db0b6a4aabf0e8e98fd4aab45
asim <asimjaipuri96@gmail.com>
2022-08-03 16:03:44 +0530

    added header and search input box

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,8 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TextInput } from "react-native";
 import React from "react";
+import { homeStyles } from "../styles/home.style";
+import avatar from "../assets/avatar.png";
+import { AntDesign } from "@expo/vector-icons";
 
 const Home = () => {
 	return (
@@ -7,11 +10,38 @@ const Home = () => {
 			style={{
 				flex: 1,
 				backgroundColor: "white",
-				alignItems: "center",
-				justifyContent: "center",
 			}}
 		>
-			<Text>Home</Text>
+			{/* Header starts */}
+			<View style={homeStyles.header}>
+				<Text style={homeStyles.headerText}>
+					Where do you want to discover?
+				</Text>
+
+				<Image source={avatar} style={homeStyles.headerImage} />
+			</View>
+			{/* Header ends */}
+
+			{/* search input starts */}
+			<View style={homeStyles.searchContainer}>
+				<View style={homeStyles.searchInputContainer}>
+					<AntDesign name="search1" color={"black"} size={20} />
+					<TextInput
+						placeholder="Enter Search"
+						placeholderTextColor={"black"}
+						style={homeStyles.searchInput}
+					/>
+				</View>
+				<View style={homeStyles.filters}>
+					<AntDesign
+						name="filter"
+						color={"black"}
+						size={20}
+						style={{ marginRight: 7 }}
+					/>
+				</View>
+			</View>
+			{/* search input ends */}
 		</View>
 	);
 };
--- styles/home.style.js
+++ styles/home.style.js
@@ -1,3 +1,44 @@
 import { StyleSheet } from "react-native";
 
-export const homeStyles = StyleSheet.create({});
+export const homeStyles = StyleSheet.create({
+	header: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+		marginHorizontal: 15,
+		marginTop: 40,
+	},
+	headerText: {
+		fontSize: 22,
+		color: "black",
+		fontFamily: "Nunito_600SemiBold",
+		width: "60%",
+	},
+	headerImage: {
+		height: 30,
+		width: 30,
+		borderRadius: 15,
+	},
+	searchContainer: {
+		marginTop: 15,
+		marginHorizontal: 15,
+		borderColor: "black",
+		borderWidth: 1,
+		padding: 10,
+		borderRadius: 25,
+		opacity: 0.4,
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+	},
+	searchInputContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+	},
+	searchInput: {
+		marginLeft: 10,
+		fontFamily: "Nunito_600SemiBold",
+		width: "75%",
+		color: "black",
+	},
+});
